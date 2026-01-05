a53febdf9344f9009051ef3134997c681302a713
ShreyashV <shreyashvaigankar125@gmail.com>
2026-01-05 23:02:32 +0530

    added schema design and bin level , Messed up the UI too!

--- UI_website/src/pages/LoginPage.tsx
+++ UI_website/src/pages/LoginPage.tsx
@@ -1,5 +1,6 @@
 import BrandingSection from "../components/BrandingSection"
 import LoginForm from "../components/LoginForm"
+import "../style/LoginPage.css"
 function LoginPage(){
     return(
         <div className="Login-container">
