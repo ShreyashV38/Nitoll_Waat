8a7b55e82a00552c676e134bccbc079ada030d80
ShreyashV <shreyashvaigankar125@gmail.com>
2026-01-09 10:46:07 +0530

    Recent Updates Pushed

--- UI_website/src/components/BrandingSection.tsx
+++ UI_website/src/components/BrandingSection.tsx
@@ -1,12 +1,9 @@
 import React from "react"
 import Goa from "../assets/Goa.png"
-import Garbage from "../assets/Garbage.png"
 import "../style/BrandingSection.css"
 function BrandingSection(){
     return <div className="brandingSection">
         <img className="BrandLogo" src={Goa} alt="BrandLogo" />
-        <img className="GarbageBin" src={Garbage} alt="GarbageBin" />
-        <img  className="GarbageBinSmall" src={Garbage} alt="GarbageBin" />
     </div>
 }
 export default BrandingSection
