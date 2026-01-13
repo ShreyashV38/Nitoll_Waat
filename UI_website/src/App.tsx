8363ff5f9ac02c15a0658286c1683bc4fcfa3b0f
aaknksha <aakankshaaa1503@gmail.com>
2026-01-13 18:27:39 +0530

    Add new routes for Dashboard, Vehicles, and Reports

--- UI_website/src/App.tsx
+++ UI_website/src/App.tsx
@@ -3,6 +3,10 @@ import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage'; 
 import './style/App.css';
 import AreaPage from './pages/AreaPage';
+import Dashboard from './pages/Dashboard';
+import Vehicles from './pages/Vehicles';
+import RoutesPage from './pages/RoutesPage';
+import Reports from './pages/Reports';
 
 function App() {
   return (
@@ -11,9 +15,13 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path ="/area" element={<AreaPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+         <Route path="/routes" element={<RoutesPage />} />
+        <Route path="/vehicles" element={<Vehicles />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/profile" element={<div>Profile Page</div>} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
